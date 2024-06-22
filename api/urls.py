ff5f1863fdb799546ec5690dc62386438b46f295
Massimo Ratano <massimoratano@MacBook-Pro-di-Massimo.local>
2024-06-22 19:41:45 +0200

    aggiunta librerie mongo

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.api),
+    path('', views.api2),
     path('altropercorso', views.api), # 3.121.138.195:8000/api/altropercorso
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 from .models import Api1 # E' LA CLASSE CHE CONTIENE I DATI
 
 # Create your views here.
@@ -7,3 +8,6 @@ def api(request):
     api1 = Api1.objects.all()  # vengono presi tutti i dati del database
     return render(request, 'api/pagina.html', {'api1': api1}) #dict CONTEST
  # i dati del databae vengono passati alla pagina
+
+def api2(request):
+    return HttpResponse("Pagina di Api2") 
